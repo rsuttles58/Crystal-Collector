ab39d28f58cb3fafff6c3c31c5b6d3dc56c80c2f
rsuttles58 <robert.suttles58@gmail.com>
2019-02-18 19:29:19 -0600

    Updated js onclicks to jquery, rearranged html elements.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,3 @@
-
-//******Functions********//
-function gemNumberGen() {
-    redGem = Math.floor(Math.random * 12);
-    greenGem = Math.floor(Math.random * 12);
-    purpleGem = Math.floor(Math.random * 12);
-    blueGem = Math.floor(Math.random * 12);
-    totalGoal = Math.floor(Math.random * 101 + 19);
-}
-
 $(document).ready(function () {
     //******Variables********//
     //Define base variables.  Goal, Current Score, wins, losses, and the four crystals.
@@ -15,13 +5,13 @@ $(document).ready(function () {
     var score = 0;
     var wins = 0;
     var losses = 0;
-    var redGem = 0;
-    var greenGem = 0;
-    var purpleGem = 0;
-    var blueGem = 0;
+    var redGem = Math.floor(Math.random * 12);
+    var greenGem = Math.floor(Math.random * 12);
+    var purpleGem = Math.floor(Math.random * 12);
+    var blueGem = Math.floor(Math.random * 12);
 
-    Math.floor()
-    Math.random()
+    Math.floor();
+    Math.random();
     //call gemNumberGen which is used to call and set the values of the four crystals and the goal.  This does not appear to be working.
     gemNumberGen();
 
@@ -35,18 +25,35 @@ $(document).ready(function () {
     $("#red").click(function () {
         redGem = parseInt(redGem);
         score = score + redGem;
+        $("#currentScore").text(score);
+        console.log(redGem);
     })
     $("#green").click(function () {
         greenGem = parseInt(greenGem);
         score = score + greenGem;
+        $("#currentScore").text(score);
+        console.log(redGem);
     })
     $("#purple").click(function () {
         purpleGem = parseInt(purpleGem);
         score = score + purpleGem;
+        $("#currentScore").text(score);
+        console.log(redGem);
     })
     $("#blue").click(function () {
         blueGem = parseInt(blueGem);
         score = score + blueGem;
+        $("#currentScore").text(score);
+        console.log(redGem);
     })
 
 })
+
+//******Functions********//
+function gemNumberGen() {
+    redGem = Math.floor(Math.random * 12);
+    greenGem = Math.floor(Math.random * 12);
+    purpleGem = Math.floor(Math.random * 12);
+    blueGem = Math.floor(Math.random * 12);
+    totalGoal = Math.floor(Math.random * 101 + 19);
+}
