c8264df924b6fc2a1764db8291df1697220ba9e6
rsuttles58 <robert.suttles58@gmail.com>
2019-02-20 22:57:39 -0600

    Tied up code, got audio file working.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,12 @@
 $(document).ready(function () {
     //******Variables********//
     //Define base variables.  Goal, Current Score, wins, losses, and the four crystals.
-
-    var wins = 0;
-    var losses = 0;
     var redGem = Math.floor(Math.random() * 12);
     var greenGem = Math.floor(Math.random() * 12);
     var purpleGem = Math.floor(Math.random() * 12);
     var blueGem = Math.floor(Math.random() * 12);
     var displayGoal;
-    var market = document.getElementById("audio"); 
 
-    // Math.floor();
-    // Math.random();
     //call gemNumberGen which is used to call and set the values of the four crystals and the goal.  This does not appear to be working.
     gemNumberGen();
     goal();
@@ -28,40 +22,39 @@ $(document).ready(function () {
         redGem = parseInt(redGem);
         score = score + redGem;
         $("#currentScore").text(score);
-        console.log(redGem);
         scoreEval();
     })
     $("#green").click(function () {
         greenGem = parseInt(greenGem);
         score = score + greenGem;
         $("#currentScore").text(score);
-        console.log(greenGem);
         scoreEval();
     })
     $("#purple").click(function () {
         purpleGem = parseInt(purpleGem);
         score = score + purpleGem;
         $("#currentScore").text(score);
-        console.log(purpleGem);
         scoreEval();
     })
     $("#blue").click(function () {
         blueGem = parseInt(blueGem);
         score = score + blueGem;
         $("#currentScore").text(score);
-        console.log(blueGem);
         scoreEval();
     })
 
 
 })
+//I think there is some fundamental flaw in how I think about document.ready.  Need to study up on the usage, best practices, and flaws to watch out for.
 var score = 0;
+var wins = 0;
+var losses = 0;
 //******Functions********//
 function gemNumberGen() {
-    redGem = Math.floor(Math.random() * 12);
-    greenGem = Math.floor(Math.random() * 12);
-    purpleGem = Math.floor(Math.random() * 12);
-    blueGem = Math.floor(Math.random() * 12);
+    redGem = Math.floor(Math.random() * 12 + 1);
+    greenGem = Math.floor(Math.random() * 12 + 1);
+    purpleGem = Math.floor(Math.random() * 12 + 1);
+    blueGem = Math.floor(Math.random() * 12 + 1);
     displayGoal = Math.floor(Math.random() * 101 + 19);
 }
 
@@ -69,38 +62,24 @@ function goal() {
     displayGoal = Math.floor(Math.random() * 101 + 19);
     $("#goal").text(displayGoal);
 }
-
+//Resetting the default position state.
 function gameReset() {
+    goal();
     gemNumberGen();
     score = 0;
-}
-
-function loss(){
-    losses = losses + 1;
+    $("#currentScore").text(score);
+    $("#goal").text(displayGoal);
     $("#losses").text(losses);
-}
-
-function win(){
-    wins = wins + 1;
     $("#wins").text(wins);
 }
-
 function scoreEval() {
     if (displayGoal === score) {
         alert("You won!");
-        win();
+        wins++;
         gameReset();
     } else if (score > displayGoal) {
         alert("Sorry, you lost this time.");
-        loss();
+        losses = losses + 1;
         gameReset();
     }
 }
-
-function playAudio() { 
-    market.play(); 
-  } 
-  
-  function pauseAudio() { 
-    market.pause(); 
-  } 
