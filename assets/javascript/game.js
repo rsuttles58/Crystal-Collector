95c035e07db4d014bb63504551956690eb3e2321
rsuttles58 <robert.suttles58@gmail.com>
2019-02-21 20:26:16 -0600

    Small code fix related to gems.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,55 @@
+//*********Variables**********//
+
+var score = 0;
+var wins = 0;
+var losses = 0;
+var redGem = Math.floor(Math.random() * 12 + 1);
+var greenGem = Math.floor(Math.random() * 12 + 1);
+var purpleGem = Math.floor(Math.random() * 12 + 1);
+var blueGem = Math.floor(Math.random() * 12 + 1);
+var displayGoal;
+
+//I think there is some fundamental flaw in how I think about document.ready.  Need to study up on the usage, best practices, and flaws to watch out for.
+
+//******Functions********//
+function gemNumberGen() {
+    redGem = Math.floor(Math.random() * 12 + 1);
+    greenGem = Math.floor(Math.random() * 12 + 1);
+    purpleGem = Math.floor(Math.random() * 12 + 1);
+    blueGem = Math.floor(Math.random() * 12 + 1);
+    displayGoal = Math.floor(Math.random() * 101 + 19);
+}
+
+function goal() {
+    displayGoal = Math.floor(Math.random() * 101 + 19);
+    $("#goal").text(displayGoal);
+}
+//Resetting the default position state.
+function gameReset() {
+    goal();
+    gemNumberGen();
+    score = 0;
+    $("#currentScore").text(score);
+    $("#goal").text(displayGoal);
+    $("#losses").text(losses);
+    $("#wins").text(wins);
+}
+
+function scoreEval() {
+    if (displayGoal === score) {
+        alert("You won!");
+        wins++;
+        gameReset();
+    } else if (score > displayGoal) {
+        alert("Sorry, you lost this time.");
+        losses = losses + 1;
+        gameReset();
+    }
+}
+
 $(document).ready(function () {
     //******Variables********//
     //Define base variables.  Goal, Current Score, wins, losses, and the four crystals.
-    var redGem = Math.floor(Math.random() * 12);
-    var greenGem = Math.floor(Math.random() * 12);
-    var purpleGem = Math.floor(Math.random() * 12);
-    var blueGem = Math.floor(Math.random() * 12);
-    var displayGoal;
 
     //call gemNumberGen which is used to call and set the values of the four crystals and the goal.  This does not appear to be working.
     gemNumberGen();
@@ -43,43 +87,4 @@ $(document).ready(function () {
         scoreEval();
     })
 
-
 })
-//I think there is some fundamental flaw in how I think about document.ready.  Need to study up on the usage, best practices, and flaws to watch out for.
-var score = 0;
-var wins = 0;
-var losses = 0;
-//******Functions********//
-function gemNumberGen() {
-    redGem = Math.floor(Math.random() * 12 + 1);
-    greenGem = Math.floor(Math.random() * 12 + 1);
-    purpleGem = Math.floor(Math.random() * 12 + 1);
-    blueGem = Math.floor(Math.random() * 12 + 1);
-    displayGoal = Math.floor(Math.random() * 101 + 19);
-}
-
-function goal() {
-    displayGoal = Math.floor(Math.random() * 101 + 19);
-    $("#goal").text(displayGoal);
-}
-//Resetting the default position state.
-function gameReset() {
-    goal();
-    gemNumberGen();
-    score = 0;
-    $("#currentScore").text(score);
-    $("#goal").text(displayGoal);
-    $("#losses").text(losses);
-    $("#wins").text(wins);
-}
-function scoreEval() {
-    if (displayGoal === score) {
-        alert("You won!");
-        wins++;
-        gameReset();
-    } else if (score > displayGoal) {
-        alert("Sorry, you lost this time.");
-        losses = losses + 1;
-        gameReset();
-    }
-}
